d5732713e0a7b5070be065406ff168a98f0361a4
dibkb <dibas9110@gmail.com>
2025-03-27 22:22:28 +0530

    headers

--- src/reddit.py
+++ src/reddit.py
@@ -14,27 +14,34 @@ class RedditPost:
     def __init__(self, url):
         self.url = url + ".json"
         self.headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0',
+            'Accept': 'application/json',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive',
+            'DNT': '1'
         }
         self.comments = []
 
     def get_comments(self):
         if len(self.comments) > 0:
             return self.comments
+        
         response = requests.get(self.url, headers=self.headers)
         if response.status_code == 200:
             data = response.json()
             comments = data[1]['data']['children']
             
-        for comment in comments:
-            comment_body = comment['data'].get('body')
-            if comment_body:
-                self.comments.append(comment_body.strip())
-                if len(self.comments) == 6:
-                    break
+            for comment in comments:
+                comment_body = comment['data'].get('body')
+                if comment_body:
+                    self.comments.append(comment_body.strip())
+                    if len(self.comments) == 6:
+                        break
+            return self.comments
         else:
             logger.error(f"Failed to fetch data: {response.status_code}")
-        return self.comments
+            return self.comments
 
 
     
